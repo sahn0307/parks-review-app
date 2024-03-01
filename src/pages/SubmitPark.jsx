62f7e5fb1188eb3579368926b3bcdc69254ae105
Gabriella Richmond <gabriella.m.richmond@gmail.com>
2024-03-01 13:45:26 -0800

    added for to submit page

--- src/pages/SubmitPark.jsx
+++ src/pages/SubmitPark.jsx
@@ -1,10 +1,43 @@
 import NavBar from "../components/NavBar";
+import { useState } from 'react';
 
 function SubmitPark() {
+  const [formData, setFormData] = useState({
+    name: "",
+    image: "",
+    location: "",
+    description: ""
+  });
+  
+  
+
   return(
     <>
       <NavBar />
       <h1>Submit a Park!</h1>
+      <form>
+        <input
+          type="text"
+          name="name"
+          placeholder="Park name"
+        />
+        <input
+          type="text"
+          name="image"
+          placeholder="Image URL"
+        />
+        <input
+          type="text"
+          name="location"
+          placeholder="Park location (State)"
+        />
+        <input
+          type="text"
+          name="description"
+          placeholder="Park description (optional)"
+        />
+        <button type="submit">Add Park</button>
+      </form>
     </>
   )
 }
