34ba873ab87b8be3f7a654a7c27a5b0644ddd4ca
Gabriella Richmond <gabriella.m.richmond@gmail.com>
2024-03-01 14:51:48 -0800

    commented stuff

--- src/pages/SubmitPark.jsx
+++ src/pages/SubmitPark.jsx
@@ -2,20 +2,35 @@ import NavBar from "../components/NavBar";
 import { useState } from 'react';
 
 function SubmitPark() {
-  const [formData, setFormData] = useState({
-    name: "",
-    image: "",
-    location: "",
-    description: ""
-  });
+  // const [formData, setFormData] = useState({
+  //   name: "",
+  //   image: "",
+  //   location: "",
+  //   description: ""
+  // });
   
-  
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+
+  //   try {
+  //     const resp = await fetch ("http://localhost:3000/nationalParks", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify(formData),
+  //     });
+
+  //     const data = await resp.json();
+
+  //   }
+  // }
 
   return(
     <>
       <NavBar />
       <h1>Submit a Park!</h1>
-      <form>
+      <form className="new-park=form" > 
         <input
           type="text"
           name="name"
