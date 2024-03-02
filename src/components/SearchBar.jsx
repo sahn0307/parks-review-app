d94142b445f78a9dd9e1085e7109ab5a414309aa
Sung-Jin Ahn <sahn0307@gmail.com>
2024-03-02 10:48:15 -0700

    add search bar to homepage

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,18 +1,16 @@
-function SearchBar() {
-
-
-  return (
-    <div className="searchbar">
-      <label htmlFor="search">Search Parks:</label>
-      <input
+function SearchBar({ searchQuery, setSearchQuery }) {
+return (
+    <div className="search-bar">
+    <label htmlFor="search">Search Parks:</label>
+    <input
         type="text"
         id="search"
+        value={searchQuery}
         placeholder="Type a name to search..."
-        value=""
-        onChange={}
-      />
+        onChange={(e) => setSearchQuery(e.target.value)}
+    />
     </div>
-  );
+);
 }
 
 export default SearchBar;
